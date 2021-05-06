ebb0a95f34b2374028218d18a7dc2a6209aa55f6
Temitope Olotin <olotintemitope@gmail.com>
2021-05-06 23:12:05 +0100

    update language file

--- resources/lang/en/qanda.php
+++ resources/lang/en/qanda.php
@@ -12,6 +12,7 @@ return [
     |
     */
 
+    'welcome.msg' => 'Welcome to interactive Q And A system',
     'question' => 'What will you like to do?',
     'question.opt_1' => 'Add Questions And Answer',
     'question.opt_2' => 'View Previous Answers',
@@ -19,5 +20,11 @@ return [
     'question.answers.ask' => 'Add answers separated by a comma. E.g (bay, bathroom, boy)',
     'question.answers.answer.ask' => 'Pls choose the answer from the options',
     'exit' => 'exit',
-    'insert_success_msg' => 'Q and A successfully added',
+    'insert_success_msg' => 'Record successfully added',
+    'question_to_practice' => 'Choose the questions to practice?',
+    'response_summary' => 'Here are the summaries of your Q and A',
+    'questions.completion_percentage' => '* So far you have completed the questions :percentage%',
+    'questions.total_completion' => '* You completed :completed question(s) out of :total question(s)',
+    'questions.correct_answer.percentage' => '* You got :percentage% answers correctly',
+    'questions.answers' => '* You got :rightAnswer question(s) right, while you got :wrongAnswer question(s) wrong',
 ];
